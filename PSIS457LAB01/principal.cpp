3898eb85609bfba7a4b3ec31a03c2f2cf803978b
kevin <kevin0oravl@gmail.com>
2022-03-31 15:50:20 -0400

    primer cambio

--- PSIS457LAB01/principal.cpp
+++ PSIS457LAB01/principal.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <locale.h>
+using namespace std;
 int main() {
-	std::cout<< "hola mundo" << std::endl;
+	setlocale(LC_CTYPE, "Spanish");
+	cout<< "Biografia" << endl;
+	cout << "Nacido el 22 de agosto del año 1997,\n estudió en la primaria Eduardo Avaroa, secuandaria en el colegio Roberto Alvarado, entró en la carrera de Ing. Electromecanica" << endl;
 
-
+	system("pause");
+	return 0;
 }
