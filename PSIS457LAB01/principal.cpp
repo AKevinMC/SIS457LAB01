e8473c16386eeea0ab853aa4dba86d1e6b552e05
AKevinMC <kevin0oravl@gmail.com>
2022-04-04 20:21:44 -0400

    Update principal.cpp
    terminado

--- PSIS457LAB01/principal.cpp
+++ PSIS457LAB01/principal.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <locale.h>
 using namespace std;
+string b = "si";
 int main() {
 	setlocale(LC_CTYPE, "Spanish");
-	cout<< "Biografia" << endl;
-	cout << "Nacido el 22 de agosto del año 1997,\n estudió en la primaria Eduardo Avaroa, secuandaria en el colegio Roberto Alvarado, entró en la carrera de Ing. Electromecanica" << endl;
+	do {
+		cout << "\nBiografia de Kevin Medrano\n\n";
+		cout << "Nació el 22 de agosto del año 1997 en Sucre, \nfamilia: Mamá, sus abuelos y hermana\n" <<
+			"Estudió en la primaria Eduardo Avaroa, \nsecuandaria en el colegio Roberto Alvarado\n" <<
+			"En la actualidad estudia en la univeridad USFX \nen la carrera de Ing de Sistemas\n" << endl;
 
-	system("pause");
+		cout << "QUIERE VOLVER A VER LA BIOGRAFÍA?    ";
+		cin >> b;
+
+	} while (b == "si");
+
 	return 0;
 }
